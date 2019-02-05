ec2702eb6aeb20f916065a514b878954e6c2cc2a
Niclas Wormann <niclas.wormann@gmail.com>
2019-02-05 19:22:54 +0100

    push it to the limit

--- src/components/bands/comp.band.single.vue
+++ src/components/bands/comp.band.single.vue
@@ -1,27 +1,24 @@
 <template>
   <section class="band-single">
-    <div class = "band-wrapper" v-for="band in bands" :key="band.bands.bId">
-      <div class = "band" v-if="band.bands.bId == bId">
-        <h1> {{ band.name }}</h1>
+    <div class = "band-wrapper" v-for="band in bands" :key="band.bId">
+      <div class = "band" v-if="bId == band.bands[bId-1].bId">
+        <div> {{ band.bands[bId-1] }} </div>
       </div>
-      <div> {{ band }}</div>
+      <!-- <div> {{ band.bands.bId }}</div> -->
     </div>
     <div> {{ bId }}</div>
   </section>
 </template>
 
 <script>
+import bandJson from '../../assets/bandSinglePageJson/bands.json'
 export default {
   name: 'Band',
   data () {
     return {
       bId: this.$route.params.bId,
       name: this.$route.params.name,
-      bands: [require('../../assets/bandSinglePageJson/bands.json')]
-    }
-  },
-  methods: {
-    getData () {
+      bands: [bandJson]
     }
   }
 }
