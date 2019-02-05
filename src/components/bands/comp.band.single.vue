8920074146010983bf7a7f3ac4686ae7cb28ffd6
Niclas Wormann <niclas.wormann@gmail.com>
2019-02-05 18:30:00 +0100

    changes

--- src/components/bands/comp.band.single.vue
+++ src/components/bands/comp.band.single.vue
@@ -1,18 +1,27 @@
 <template>
   <section class="band-single">
-    <div > ID: {{ bId }} Name: {{ bandObj }}</div>
+    <div class = "band-wrapper" v-for="band in bands" :key="band.bands.bId">
+      <div class = "band" v-if="band.bands.bId == bId">
+        <h1> {{ band.name }}</h1>
+      </div>
+      <div> {{ band }}</div>
+    </div>
+    <div> {{ bId }}</div>
   </section>
 </template>
 
 <script>
-
 export default {
   name: 'Band',
   data () {
     return {
       bId: this.$route.params.bId,
       name: this.$route.params.name,
-      bandObj: require('../../assets/bandSinglePageJson/bands.json')
+      bands: [require('../../assets/bandSinglePageJson/bands.json')]
+    }
+  },
+  methods: {
+    getData () {
     }
   }
 }
