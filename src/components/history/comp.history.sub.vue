78fde9ddf535294df6b69ada81860ec48cb44171
Niclas Wormann <niclas.wormann@gmail.com>
2019-02-15 10:44:06 +0100

    css update

--- src/components/history/comp.history.sub.vue
+++ src/components/history/comp.history.sub.vue
@@ -58,8 +58,34 @@ export default {
 
 <style>
 .history-slider {
-  text-align: center;
-  padding: 2rem;
+  position: relative;
+  border: 2px solid var(--beige);
+  padding: 2.5rem;
+  transition: all 200ms ease-in-out;
+}
+.history-slider::after {
+  content: '';
+  width: 100%;
+  position: absolute;
+  height: 100%;
+  z-index: -1;
+  left: 2%;
+  top: 2%;
+  border: 2px solid var(--gold);
+  background-color: transparent;
+}
+.history-slider::before {
+  content: '';
+  background-image: url('../../assets/images/dmed2019_web_bg_blank.jpg');
+  width: 95%;
+  background-size: 350%;
+  background-position: center;
+  position: absolute;
+  height: 95%;
+  z-index: -1;
+  left: 2.5%;
+  top: 2.5%;
+  opacity: 0.2;
 }
 .image-slider {
   display: grid;
