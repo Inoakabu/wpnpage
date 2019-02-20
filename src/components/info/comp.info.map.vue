9759f5e8a8efeb32d584370bf7b4e3dde3fb6118
Niclas Wormann <niclas.wormann@gmail.com>
2019-02-20 13:10:26 +0100

    push update before deploy

--- src/components/info/comp.info.map.vue
+++ src/components/info/comp.info.map.vue
@@ -4,7 +4,7 @@
     <div class="map">
       <l-map :zoom="zoom" :center="center">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        <l-marker v-for="marker in markers" :key="marker.name" :lat-lng="marker.pos" :icon="marker.icon" >
+        <l-marker v-for="marker in markers" :key="marker.name" :lat-lng="marker.pos" :icon="marker.icon" @click="goTo">
           <l-tooltip :content="marker.tooltip" />
         </l-marker>
       </l-map>
@@ -15,7 +15,6 @@
 <script>
 import { L, LMap, LTileLayer, LMarker, LTooltip } from 'vue2-leaflet'
 import 'leaflet/dist/leaflet.css'
-// import mapJson from '../../assets/info/map.json'
 export default {
   components: { LMap, LTileLayer, LMarker, LTooltip },
   data () {
@@ -24,32 +23,24 @@ export default {
       center: L.latLng(52.48841, 13.38858),
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      hotelIcon: L.icon({
-        iconUrl: require('../../assets/info/sharp-hotel-24px.svg'),
-        iconAnchor: [22, 94],
-        iconSize: [38, 95]
-      }),
-      venueIcon: L.icon({
-        iconUrl: require('../../assets/info/sharp-local_activity-24px.svg'),
-        iconAnchor: [22, 94],
-        iconSize: [38, 95]
-      }),
-      markers: [ // mapJson.markers
+      markers: [
         {
           name: 'De Mortem Et Diabolum',
           pos: L.latLng(52.48462, 13.39142),
           tooltip: 'De Mortem Et Diabolum',
           icon: L.icon({
             iconUrl: require('../../assets/info/sharp-local_activity-24px.svg')
-          })
+          }),
+          link: ''
         },
         {
-          name: 'Hotel',
+          name: 'Metropol Hostel',
           pos: L.latLng(52.49381, 13.38793),
-          tooltip: 'Hotel',
+          tooltip: 'Metropol Hostel',
           icon: L.icon({
             iconUrl: require('../../assets/info/sharp-hotel-24px.svg')
-          })
+          }),
+          link: 'https://www.google.de/maps/place/Metropol+Hostel+Berlin/@52.4938521,13.385581,17z/data=!3m1!4b1!4m5!3m4!1s0x47a8502922fa4a61:0x9439b531e7d32206!8m2!3d52.4938489!4d13.3877697'
         },
         {
           name: 'atm',
@@ -57,7 +48,8 @@ export default {
           tooltip: 'ATM',
           icon: L.icon({
             iconUrl: require('../../assets/info/sharp-local_atm-24px.svg')
-          })
+          }),
+          link: ''
         },
         {
           name: 'train',
@@ -65,9 +57,15 @@ export default {
           tooltip: 'Train Station',
           icon: L.icon({
             iconUrl: require('../../assets/info/sharp-train-24px.svg')
-          })
+          }),
+          link: ''
         }
       ]
+    }
+  },
+  methods: {
+    goTo () {
+      return open(this.markers.link)
     }
   }
 }
