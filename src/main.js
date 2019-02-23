da6068fc56345d1928903ff8c128a24b8ed2d45b
Stefan <s.mairbaeurl@gmail.com>
2019-02-23 18:27:37 +0100

    leaflet fixxi

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './routes'
-// import L from 'leaflet'
-//
-// delete L.Icon.Default.prototype._getIconUrl
-//
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: require('~leaflet/dist/images/marker-icon-2x.png'),
-//   iconUrl: require('~leaflet/dist/images/marker-icon.png'),
-//   shadowUrl: require('~leaflet/dist/images/marker-shadow.png')
-// })
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  },
   render: h => h(App)
 }).$mount('#app')
