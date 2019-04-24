ddf5ad272e9ff1f8005a7d502275471e71e876fa
Niclas Wormann <niclas.wormann@gmail.com>
2019-04-24 10:58:22 +0200

    news page styled

--- src/routes/route.news.vue
+++ src/routes/route.news.vue
@@ -1,10 +1,11 @@
 <template>
-  <section class="news-single">
-      <div class="news-inner-single" v-if="news">
-        <h1> {{ news.title }}</h1>
-        <div> {{ news.text[currentLang] }}</div>
-        <img :src="imageSrc"/>
+  <section class="news-page" v-if="news">
+      <h1 class="news-page-headline">{{ news.title }}</h1>
+      <p class="news-page-description">{{ news.text[currentLang] }}</p>
+      <div class="div-image">
+        <img class="news-page-image" :src="imageSrc"/>
       </div>
+      <router-view></router-view>
   </section>
 </template>
 
@@ -31,5 +32,35 @@
 </script>
 
 <style>
+.news-page {
+    text-align: center;
+  }
+  .news-page-headline {
+    text-align: center;
+    margin: 5rem 0;
+  }
 
+  .news-page-description {
+    line-height: 1.5rem;
+    padding-top: 10rem;
+    text-align: center;
+  }
+
+  .news-page-video {
+    padding-top: 5rem;
+  }
+
+  .news-page-social {
+    display: inline-flex;
+    padding: 1rem;
+    align-self: center;
+  }
+
+  .news-page-image{
+    max-width: 1024px;
+    text-align: center
+  }
+  .div-image{
+    text-align: center
+  }
 </style>
