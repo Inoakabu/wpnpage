c2d63af0874a31f9607b0f5a76bff52382a3817d
Inoakabu <wormann@semicode.de>
2019-12-11 11:31:06 +0100

    push

--- src/components/runningorder/comp.runningorder.vue
+++ src/components/runningorder/comp.runningorder.vue
@@ -1,19 +1,31 @@
 <template>
   <section class="bands">
     <h2>Running Order</h2>
-    <div class="list four">
-      <runningorderListComp/>
+    <div class="runningorder">
+      
     </div>
     
   </section>
 </template>
 
 <script>
-import runningorderListComp from '../runningorder/comp.runningorder.list'
+// import runningorderListComp from '../runningorder/comp.runningorder.list'
+import roPictures from ''
 export default {
-  components: {
-    runningorderListComp
-  }
+  // components: {
+  //   runningorderListComp
+  // }
+  data () {
+    return {
+      roFriday: require('../../assets/images/runningorder/RO_Friday.jpg'),
+      roSaturday: require('../../assets/images/runningorder/RO_Saturday.jpg')
+    }
+  },
+  methods: {
+    test () {
+      console.log(roPictures)
+    }
+  },
 }
 </script>
 
