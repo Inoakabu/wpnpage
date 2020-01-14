bc7634e8944330adc5bb59374329a195dae18b38
Inoakabu <wormann@semicode.de>
2020-01-14 14:27:45 +0100

    start implementing cockpit cms api

---
+++ src/components/fetcher/fetchDB.vue
@@ -0,0 +1,18 @@
+<script>
+
+// import axios from 'axios'
+import { useFetch } from "vue-async-function";
+
+import cpAPICall from '../../assets/conf/cpAPI.json'
+
+export default {
+  setup() {
+    console.log("TEst")
+    const apiCall = cpAPICall.call
+    const { data, error, isLoading } = useFetch(apiCall.apiURL + apiCall.token, {
+      heders: { Accept: "appliction/json" }
+    });
+    return { data, error, isLoading }
+  }
+}
+</script>
